39e81dbbee4226ea7c11e6ef1fce9633a5605db9
Carlos Morfin Diaz <carlosmorfindiaz@gmail.com>
2019-08-07 21:35:12 -0500

    Create User model

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,17 @@ module Bookstore
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Internationalization and localization config
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.default_locale = :'es-MX
+    config.i18n.default_locale = :'es-MX'
+
+    config.time_zone = 'Mexico City'
+    config.active_record.time_zone_aware_types = [:datetime, :time]
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    config.active_record.belongs_to_required_by_default = false
+    config.active_record.pluralize_table_names = false
   end
 end
