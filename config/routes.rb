414b7467569396eb5a647b4d83b5923510c7357c
Carlos Morfin Diaz <carlosmorfindiaz@gmail.com>
2019-08-08 00:06:23 -0500

    Configuration and add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users,
+    controllers: {
+      sessions:  'public/users/sessions'
+    }
+
+  devise_scope :user do
+    root 'public/users/sessions#new'
+  end
+
   namespace :admin do
   end
 
